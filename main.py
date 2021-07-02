819f5560fbaf8ffe7f6a99656d788eb1d1a14f2f
Kawan Henrique <kawan.inf@gmail.com>
2021-07-02 17:14:35 -0300

    Layout Feito

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ from kivy.core.window import Window
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDFlatButton
 
-Window.size = (360, 640)
+Window.size = (360, 540)
 
 class Tela(MDBoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def validando_operador(self, operador, conta):
+        if len(conta) > 0 and conta[-1] in "+-/*" :
+            return conta
+        return conta+operador
+
+    def somar_conta(self, conta):
+        operadores = {"*":0, "/":0, "+":0, "-":0,}
+        def separar_numeros(numero):
+            ...
+
+
+
 class Calculadora(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Red"
-        self.theme_cls.primary_hue = "900"
+        self.theme_cls.primary_hue = "500"
         print(self.theme_cls.primary_color)
         return Tela()
 
